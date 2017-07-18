f01d8a7267036665db8c7553b9b8ae09ee832430
Ta-Wei <ta-wei.wang@cern.ch>
2017-07-19 01:01:21 +0200

    update B to D configs

---
+++ Run2Ana/BtoDPbPb/python/Pythia8_BuToD0Pi_Bpt0p0_Pthat30_TuneCUEP8M1_5020GeV.py
@@ -0,0 +1,62 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+from GeneratorInterface.EvtGenInterface.EvtGenSetting_cff import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+                         pythiaPylistVerbosity = cms.untracked.int32(0),
+                         pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         comEnergy = cms.double(5020.0),
+                         maxEventsToPrint = cms.untracked.int32(0),
+                         ExternalDecays = cms.PSet(
+        EvtGen130 = cms.untracked.PSet(
+            decay_table = cms.string('GeneratorInterface/EvtGenInterface/data/DECAY_2010.DEC'),
+            particle_property_file = cms.FileInPath('GeneratorInterface/EvtGenInterface/data/evt.pdl'),
+            user_decay_file = cms.vstring('GeneratorInterface/ExternalDecays/data/Bu_D0Pi_KPiPi.dec'),
+            list_forced_decays = cms.vstring('MyB+',
+                                              'MyB-'),
+            operates_on_particles = cms.vint32()
+            ),
+        parameterSets = cms.vstring('EvtGen130')
+        ),
+                         PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        processParameters = cms.vstring(
+            'HardQCD:all = on',
+            'PhaseSpace:pTHatMin = 30.',
+            
+            ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CUEP8M1Settings',
+                                    'processParameters',
+                                    )
+        )
+                         )
+
+generator.PythiaParameters.processParameters.extend(EvtGenExtraParticles)
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.1 $'),
+    name = cms.untracked.string('$Source: Configuration/Generator/python/BuToKstarMuMu_forSTEAM_13TeV_TuneCUETP8M1_cfi.py $'),
+    annotation = cms.untracked.string('Summer14: Pythia8+EvtGen130 generation of Bu --> K* Mu+Mu-, 13TeV, Tune CUETP8M1')
+    )
+
+###########
+# Filters #
+###########
+BD0Daufilter = cms.EDFilter("PythiaMomDauFilter",
+    ParticleID = cms.untracked.int32(521),
+    MomMinPt = cms.untracked.double(0.),
+    MomMinEta = cms.untracked.double(-2.4),
+    MomMaxEta = cms.untracked.double(2.4),
+    DaughterIDs = cms.untracked.vint32(-421, 211),
+    NumberDaughters = cms.untracked.int32(2),
+    DaughterID = cms.untracked.int32(-421),
+    DescendantsIDs = cms.untracked.vint32(321, -211),
+    NumberDescendants = cms.untracked.int32(2),
+    MinEta = cms.untracked.double(-2.5),
+    MaxEta = cms.untracked.double(2.5),
+)
+
+ProductionFilterSequence = cms.Sequence(generator*BD0Daufilter)
